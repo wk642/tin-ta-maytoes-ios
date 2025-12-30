6e743203f0f88e5352dec783f7e62528635be372
wk642 <w642kelley@gmail.com>
2025-12-30 14:33:43 -0600

    Initial Commit

---
+++ Tin-Ta-Maytoes/Tin_Ta_MaytoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tin_Ta_MaytoesApp.swift
+//  Tin-Ta-Maytoes
+//
+//  Created by Winnie Kelley on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Tin_Ta_MaytoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
